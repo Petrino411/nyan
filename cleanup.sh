95b0697c1bb4294c459035987182ea4c8bba8155
Пётр Сивунов <petr.siv@rambler.ru>
2025-12-15 11:02:31 +0300

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,12 +8,12 @@ cd "$(dirname "$0")"
 MONGO_DATA="/mnt/data/mongocontainer"
 
 echo "[mongo-recreate] Stopping mongo container..."
-docker-compose stop mongo
+docker compose stop mongo
 
 echo "[mongo-recreate] Removing MongoDB data..."
 rm -rf ${MONGO_DATA:?}/*
 
 echo "[mongo-recreate] Starting mongo container..."
-docker-compose up -d mongo
+dockern compose up -d mongo
 
 echo "[mongo-recreate] Done."
